abe7000b334369c70c4721f2db2988a145970b11
leehong <23883175@qq.com>
2024-10-08 13:50:25 +0800

    commit new game

---
+++ StreamMatch/src/settings.bff10.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"14e41"}};
