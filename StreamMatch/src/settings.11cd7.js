04c7c8dbcd19c40de29cd42bc87fa92c2814b9ab
leehong <23883175@qq.com>
2024-10-09 13:23:09 +0800

    delete game

--- StreamMatch/src/settings.11cd7.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",main:"a155e"}};
