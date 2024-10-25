fbac71c0c52aee1d3f576909ad36948ef300f0d0
vincent <vincent@yolofi.io>
2024-10-25 14:18:12 +0700

    fix: include surge run in third party enum

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
 import { game as mathCraft } from "./mathcraft";
-// no import of surge run because it's not used in stream drop launch
+import { game as surgeRun } from "./surge-run";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   mathCraft.id,
+  surgeRun.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
