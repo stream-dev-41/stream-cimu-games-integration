8fce4140af3686ce188ab44ceddcda67f639dcda
vincent <vincent@yolofi.io>
2024-10-25 15:20:15 +0700

    fix: temporarily remove mathcraft

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
-import { game as mathCraft } from "./mathcraft";
 import { game as surgeRun } from "./surge-run";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
-  mathCraft.id,
   surgeRun.id,
   // add more games here
 ]);
