0039c34b736b93ab830a7b34d04f7bbdf7360625
vincent <vincent@yolofi.io>
2024-11-01 10:20:42 +0700

    fix: add mathcraft exp

--- src/common.ts
+++ src/common.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
 import { game as surgeRun } from "./surge-run";
+import { game as mathCraft } from "./mathcraft";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   surgeRun.id,
+  mathCraft.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
