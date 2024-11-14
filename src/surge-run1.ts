613a90b419e6d891d6fd3088ff7d639fce432541
leehong <23883175@qq.com>
2024-11-14 15:32:41 +0800

    add run2 zod file

---
+++ src/surge-run1.ts
@@ -0,0 +1,82 @@
+import { z } from "zod";
+
+export const gameParams = z.object({
+  bestScores: z
+    .number()
+    .int()
+    .min(0)
+    .describe("used to display user's personal best score"),
+  device: z.enum(["mobile", "desktop"]),
+});
+export type SurgeRunGameParams = z.infer<typeof gameParams>;
+
+export const message = z
+  .discriminatedUnion("kind", [
+    z.object({
+      kind: z
+        .literal("[game]:initialized")
+        .describe(
+          "Should be the first event in the sequence, tells Stream when to send initial params",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[host]:initial-params")
+        .describe("Setup game with game params, after initialised"),
+      userId: z.string().uuid("unique userId"),
+      sessionId: z.string().describe("unique for each game session"),
+      gameParams,
+    }),
+    z.object({
+      kind: z
+        .literal("[game]:is-ready")
+        .describe(
+          "Sent after the game has been fully setup include loading asset/logic/etc...In other words, ready to play",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[host]:start-game")
+        .describe(
+          "Start the game immediately, there should be no delay time after this event is sent to let the players play the game",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[game]:ended")
+        .describe(
+          "Game time is up or the player finishes early, then this event is sent",
+        ),
+      scores: z.number().nonnegative().int(),
+    }),
+    z.object({
+      kind: z
+        .literal("[host]:key")
+        .describe(
+          "Send key event to the game, e.g. keyboard or controller",
+        ),
+    }),
+  ])
+  .and(
+    z.object({
+      version: z
+        .literal(1)
+        .describe(
+          "this is to make sure our code knows how to handle if schema updated",
+        ),
+    }),
+  );
+export type SurgeRunMessage = z.infer<typeof message>;
+
+export const game = {
+  id: "CIMU_SURGE_RUN",
+  url: "https://stream-run.342games.com",
+  name: "Surge Run",
+  shortDescription: "TBU",
+  message,
+
+  // will be sanitized
+  descriptionInHtml: "TBU",
+  launchInstructionInHtml: "TBU",
+  scoringRulesInHtml: "TBU",
+} as const;
