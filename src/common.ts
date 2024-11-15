1129f80e21b5ae8a752ed98246cad16cda373773
vincent <vincent@yolofi.io>
2024-11-15 10:00:00 +0700

    feat: add stream run vertical version

--- src/common.ts
+++ src/common.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
 import { game as mathCraft } from "./mathcraft";
+import { game as streamRun } from "./stream-run";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   mathCraft.id,
+  streamRun.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "./common";
 export * as cardDash from "./card-dash";
 export * as mathCraft from "./mathcraft";
 export * as surgeRun from "./surge-run";
+export * as streamRun from "./stream-run";
 export type { CardDashMessage, CardDashGameParams } from "./card-dash";
 export type { MathCraftMessage, MathCraftGameParams } from "./mathcraft";
 export type { SurgeRunMessage, SurgeRunGameParams } from "./surge-run";
+export type { StreamRunMessage, StreamRunGameParams } from "./stream-run";
