f594c27b4194a0fa54d06165a99627e888b9c707
vincent <vincent@yolofi.io>
2024-11-19 16:34:32 +0700

    fix: add surge run

--- src/common.ts
+++ src/common.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
 import { game as mathCraft } from "./mathcraft";
-import { game as matrixRun } from "./matrix-run";
+import { game as surgeRun } from "./surge-run";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   mathCraft.id,
-  matrixRun.id,
+  surgeRun.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
