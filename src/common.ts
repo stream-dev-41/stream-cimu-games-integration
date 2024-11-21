e3e808cb849677dfa454a89bce70a8f0d462451f
vincent <vincent@yolofi.io>
2024-11-21 09:34:49 +0700

    fix: re-add matrix run

--- src/common.ts
+++ src/common.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import { game as cardDash } from "./card-dash";
 import { game as mathCraft } from "./mathcraft";
-import { game as surgeRun } from "./surge-run";
+import { game as matrixRun } from "./matrix-run";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   mathCraft.id,
-  surgeRun.id,
+  matrixRun.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
