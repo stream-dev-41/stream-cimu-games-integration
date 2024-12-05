4f7b546d12bb12f9e99ea16204ccb9ef35f363af
vincent <vincent@yolofi.io>
2024-12-05 21:43:05 +0700

    chore: add new games

--- src/common.ts
+++ src/common.ts
@@ -3,12 +3,16 @@ import { game as cardDash } from "./card-dash";
 import { game as mathCraft } from "./mathcraft";
 import { game as matrixRun } from "./matrix-run";
 import { game as surgeRun } from "./surge-run";
+import { game as tumbleFall } from "./tumble-fall";
+import { game as colorRecall } from "./color-recall";
 
 export const thirdPartyExperience = z.enum([
   cardDash.id,
   mathCraft.id,
   matrixRun.id,
   surgeRun.id,
+  tumbleFall.id,
+  colorRecall.id,
   // add more games here
 ]);
 export type ThirdPartyExperienceEnum = z.infer<typeof thirdPartyExperience>;
---
+++ src/tumble-fall.ts
@@ -0,0 +1,99 @@
+import { z } from "zod";
+
+export const gameParams = z.object({
+  level: z
+    .number()
+    .nonnegative()
+    .int()
+    .min(1)
+    .max(2)
+    .describe("game difficulty"),
+});
+export type TumbleFallGameParams = z.infer<typeof gameParams>;
+
+export const message = z
+  .discriminatedUnion("kind", [
+    z.object({
+      kind: z
+        .literal("[game]:initialized")
+        .describe(
+          "Should be the first event in the sequence, tells Stream when to send initial params",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[host]:initial-params")
+        .describe("Setup game with game params, after initialised"),
+      userId: z.string().uuid("unique userId"),
+      sessionId: z.string().describe("unique for each game session"),
+      gameDurationInSeconds: z
+        .number()
+        .nonnegative()
+        .int()
+        .describe("the duration of the game in seconds"),
+      gameParams,
+    }),
+    z.object({
+      kind: z
+        .literal("[game]:is-ready")
+        .describe(
+          "Sent after the game has been fully setup include loading asset/logic/etc...In other words, ready to play",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[host]:start-game")
+        .describe(
+          "Start the game immediately, there should be no delay time after this event is sent to let the players play the game",
+        ),
+      timeLeftInSeconds: z
+        .number()
+        .nonnegative()
+        .int()
+        .describe(
+          "how many seconds left before the game will end, should be 0 <= timeLeft <= gameDurationInSeconds",
+        ),
+    }),
+    z.object({
+      kind: z
+        .literal("[game]:ended")
+        .describe(
+          "Game time is up or the player finishes early, then this event is sent",
+        ),
+      scores: z.number().nonnegative().int(),
+      falls: z.number().nonnegative().int(),
+      elapsedTimeInSeconds: z
+        .number()
+        .nonnegative()
+        .int()
+        .describe(
+          "Number of seconds elapsed since player stared the game until end or player finished it, should be 0 <= elapsed <= timeLeft",
+        ),
+    }),
+  ])
+  .and(
+    z.object({
+      version: z
+        .literal(1)
+        .describe(
+          "this is to make sure our code knows how to handle if schema updated",
+        ),
+    }),
+  );
+export type TumbleFallMessage = z.infer<typeof message>;
+
+export const game = {
+  id: "CIMU_TUMBLE_FALL",
+  url: "https://stream-four.342games.com",
+  name: "Tumble Fall",
+  shortDescription: "TBD",
+  message,
+
+  // will be sanitized
+  descriptionInHtml:
+    "Users will tumble their way different sized platforms to get the highest score possible. Size of platforms depends on difficulty level selected by creators.",
+  launchInstructionInHtml:
+    "Normal mode: Larger sized platforms\nHard mode: Smaller sized platforms",
+  scoringRulesInHtml:
+    "Fans are scored based on the number of successful jumps. Landing in the middle consecutively builds a streak. They get 250 base gems for passing and 500 bonus gems for making into Top 100.",
+} as const;
